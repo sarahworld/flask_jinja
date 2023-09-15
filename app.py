a74405fd75dfa486ad803650ae6b11dd619dd32c
shamsi <sarah.shamsi90@gmail.com>
2023-09-14 18:53:11 -0700

    Challenge Done but Further Study

---
+++ app.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template, request
+from flask_debugtoolbar import DebugToolbarExtension
+from stories import story
+
+
+app = Flask(__name__)
+app.config["SECRET_KEY"] ="secret" 
+
+debug = DebugToolbarExtension(app)
+
+@app.route("/")
+def questions():
+    prompts = story.prompts
+    return render_template("form.html", prompts=prompts)
+
+@app.route("/story")
+def make_story():
+    text = story.generate(request.args)
+    return render_template("story.html", text=text)
